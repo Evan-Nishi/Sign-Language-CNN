ea566b1a6548d2538beedcf68128ac410a5e063d
Evan Nishi <42789870+pErs0nZ@users.noreply.github.com>
2019-06-14 14:40:37 -0700

    Update vidcap.py

--- vidcap.py
+++ vidcap.py
@@ -10,8 +10,9 @@ cap = cv2.VideoCapture(0)
 def cap_img():
     ret, frame = cap.read()
     gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    resized_img =  cv2.resize(gray_scale,(28,28))
+    resized_img = cv2.resize(gray_scale,(28,28))
     cv2.imwrite('new_img.jpg',resized_img )
+
 
 root = tk.Tk()
 frame = tk.Frame(root)
